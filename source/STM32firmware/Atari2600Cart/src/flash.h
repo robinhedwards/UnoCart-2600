08f9902d66cc974d1ac2e2c39656be278a3a2012
Christian Speckner <christian.speckner@mayflower.de>
2018-03-20 21:59:17 +0100

    Checkpoint.

---
+++ source/STM32firmware/Atari2600Cart/src/flash.h
@@ -0,0 +1,18 @@
+#ifndef FLASH_H
+#define FLASH_H
+
+#include <stdint.h>
+#include <stdbool.h>
+
+typedef struct {
+    uint8_t* base;
+    uint32_t next_write_target;
+} flash_context;
+
+uint32_t avaiable_flash();
+
+bool prepare_flash(uint32_t size, flash_context *context);
+
+bool write_flash(uint32_t byte_count, uint8_t* buffer, flash_context *context);
+
+#endif // FLASH_H
