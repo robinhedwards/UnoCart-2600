921c353a96e9ae954e78111a573c5d09d79a5989
Christian Speckner <christian.speckner@mayflower.de>
2018-04-26 23:37:07 +0200

    Whitespace.

--- source/STM32firmware/Atari2600Cart/src/cartridge_3f.c
+++ source/STM32firmware/Atari2600Cart/src/cartridge_3f.c
@@ -16,89 +16,89 @@
 #define MAX_CCM_BANK (MAX_RAM_BANK + CCM_BANKS)
 
 typedef struct {
-    uint8_t max_bank;
-    uint8_t max_ram_bank;
-    uint8_t max_ccm_bank;
+	uint8_t max_bank;
+	uint8_t max_ram_bank;
+	uint8_t max_ccm_bank;
 
-    uint8_t *flash_base;
+	uint8_t *flash_base;
 } cartridge_layout;
 
 static bool setup_cartridge_image(const char* filename, uint32_t image_size, uint8_t *buffer, cartridge_layout* layout) {
-    if (image_size == 0) return false;
+	if (image_size == 0) return false;
 
-    image_size = image_size > 512 * 1024 ? 512 * 1024 : image_size;
+	image_size = image_size > 512 * 1024 ? 512 * 1024 : image_size;
 
-    uint32_t banks = image_size / 2048;
-    if (image_size % 2048) banks++;
+	uint32_t banks = image_size / 2048;
+	if (image_size % 2048) banks++;
 
-    layout->max_bank = (uint8_t)(banks - 1);
-    layout->max_ram_bank = layout->max_bank > MAX_RAM_BANK ? MAX_RAM_BANK : layout->max_bank;
+	layout->max_bank = (uint8_t)(banks - 1);
+	layout->max_ram_bank = layout->max_bank > MAX_RAM_BANK ? MAX_RAM_BANK : layout->max_bank;
 
-    if (layout->max_bank == layout->max_ram_bank) return true;
+	if (layout->max_bank == layout->max_ram_bank) return true;
 
-    layout->max_ccm_bank = layout->max_bank > MAX_CCM_BANK ? MAX_CCM_BANK : layout->max_bank;
+	layout->max_ccm_bank = layout->max_bank > MAX_CCM_BANK ? MAX_CCM_BANK : layout->max_bank;
 
-    FATFS fs;
+	FATFS fs;
 	FIL fil;
-    UINT bytes_read;
+	UINT bytes_read;
 
-    if (f_mount(&fs, "", 1) != FR_OK) goto fail_unmount;
-    if (f_open(&fil, filename, FA_READ) != FR_OK) goto fail_close;
+	if (f_mount(&fs, "", 1) != FR_OK) goto fail_unmount;
+	if (f_open(&fil, filename, FA_READ) != FR_OK) goto fail_close;
 
-    if (layout->max_ccm_bank != layout->max_bank) {
-        uint32_t flash_image_size = image_size - (layout->max_ccm_bank + 1) * 2048;
+	if (layout->max_ccm_bank != layout->max_bank) {
+		uint32_t flash_image_size = image_size - (layout->max_ccm_bank + 1) * 2048;
 
-        if (flash_image_size > available_flash()) goto fail_close;
+		if (flash_image_size > available_flash()) goto fail_close;
 
-        flash_context ctx;
+		flash_context ctx;
 
-        if (!prepare_flash(flash_image_size, &ctx)) goto fail_close;
+		if (!prepare_flash(flash_image_size, &ctx)) goto fail_close;
 
-        layout->flash_base = ctx.base;
+		layout->flash_base = ctx.base;
 
-        if (f_lseek(&fil, (RAM_BANKS + CCM_BANKS) * 2048) != FR_OK) goto fail_close;
-        uint32_t bytes_written_to_flash = 0;
+		if (f_lseek(&fil, (RAM_BANKS + CCM_BANKS) * 2048) != FR_OK) goto fail_close;
+		uint32_t bytes_written_to_flash = 0;
 
-        while (bytes_written_to_flash < flash_image_size) {
-            if (f_read(&fil, CCM_RAM, CCM_SIZE, &bytes_read) != FR_OK) goto fail_close;
+		while (bytes_written_to_flash < flash_image_size) {
+			if (f_read(&fil, CCM_RAM, CCM_SIZE, &bytes_read) != FR_OK) goto fail_close;
 
-            bytes_written_to_flash += bytes_read;
-            if (bytes_read < CCM_SIZE && bytes_written_to_flash < flash_image_size) goto fail_close;
+			bytes_written_to_flash += bytes_read;
+			if (bytes_read < CCM_SIZE && bytes_written_to_flash < flash_image_size) goto fail_close;
 
-            if (!write_flash(bytes_read, CCM_RAM, &ctx)) goto fail_close;
-        }
-    }
+			if (!write_flash(bytes_read, CCM_RAM, &ctx)) goto fail_close;
+		}
+	}
 
-    if (f_lseek(&fil, RAM_BANKS * 2048) != FR_OK) goto fail_close;
-    if (f_read(&fil, CCM_RAM, CCM_SIZE, &bytes_read) != FR_OK) goto fail_close;
+	if (f_lseek(&fil, RAM_BANKS * 2048) != FR_OK) goto fail_close;
+	if (f_read(&fil, CCM_RAM, CCM_SIZE, &bytes_read) != FR_OK) goto fail_close;
 
-    f_close(&fil);
-    f_mount(0, "", 1);
-    return true;
+	f_close(&fil);
+	f_mount(0, "", 1);
+	return true;
 
-    fail_close:
-        f_close(&fil);
-    fail_unmount:
-        f_mount(0, "", 1);
+	fail_close:
+		f_close(&fil);
+	fail_unmount:
+		f_mount(0, "", 1);
 
-    return false;
+	return false;
 }
 
 void emulate_3f_cartridge(const char* filename, uint32_t image_size, uint8_t* buffer) {
-    cartridge_layout layout;
+	cartridge_layout layout;
 
-    if (!setup_cartridge_image(filename, image_size, buffer, &layout)) return;
+	if (!setup_cartridge_image(filename, image_size, buffer, &layout)) return;
 
-    uint16_t addr, addr_prev = 0, addr_prev2 = 0, data = 0, data_prev = 0;
+	uint16_t addr, addr_prev = 0, addr_prev2 = 0, data = 0, data_prev = 0;
 	uint8_t *bankPtr = buffer;
 	uint8_t *fixedPtr;
 
-    if (layout.max_bank <= layout.max_ram_bank) fixedPtr = buffer + layout.max_bank * 2048;
-    else if (layout.max_bank <= layout.max_ccm_bank) fixedPtr = CCM_RAM + (layout.max_bank - layout.max_ram_bank - 1) * 2048;
-    else fixedPtr = layout.flash_base + (layout.max_bank - layout.max_ccm_bank - 1) * 2048;
+	if (layout.max_bank <= layout.max_ram_bank) fixedPtr = buffer + layout.max_bank * 2048;
+	else if (layout.max_bank <= layout.max_ccm_bank) fixedPtr = CCM_RAM + (layout.max_bank - layout.max_ram_bank - 1) * 2048;
+	else fixedPtr = layout.flash_base + (layout.max_bank - layout.max_ccm_bank - 1) * 2048;
 
-    if (!reboot_into_cartridge()) return;
-    __disable_irq();
+	if (!reboot_into_cartridge()) return;
+	__disable_irq();
 
 	while (1)
 	{
@@ -108,17 +108,17 @@ void emulate_3f_cartridge(const char* filename, uint32_t image_size, uint8_t* bu
 			addr_prev = addr;
 		}
 
-        // got a stable address
+		// got a stable address
 		if (addr == 0x3f)
 		{	// A12 low, read last data on the bus before the address lines change
 			while (ADDR_IN == addr) { data_prev = data; data = DATA_IN; }
 			data = data_prev>>8;
 
-            uint8_t bank = data > layout.max_bank ? data % (layout.max_bank + 1) : data;
+			uint8_t bank = data > layout.max_bank ? data % (layout.max_bank + 1) : data;
 
-            if (bank <= layout.max_ram_bank) bankPtr = buffer + bank * 2048;
-            else if (bank <= layout.max_ccm_bank) bankPtr = CCM_RAM + (bank - layout.max_ram_bank - 1) * 2048;
-            else bankPtr = layout.flash_base + (bank - layout.max_ccm_bank - 1) * 2048;
+			if (bank <= layout.max_ram_bank) bankPtr = buffer + bank * 2048;
+			else if (bank <= layout.max_ccm_bank) bankPtr = CCM_RAM + (bank - layout.max_ram_bank - 1) * 2048;
+			else bankPtr = layout.flash_base + (bank - layout.max_ccm_bank - 1) * 2048;
 		}
 		else if (addr & 0x1000)
 		{ // A12 high
--- source/STM32firmware/Atari2600Cart/src/flash.c
+++ source/STM32firmware/Atari2600Cart/src/flash.c
@@ -2,169 +2,169 @@
 
 #include "flash.h"
 
-#define RESERVED_FLASH_KB       64
+#define RESERVED_FLASH_KB	   64
 
 extern uint8_t _END_OF_FLASH;
 #define FIRST_FREE_BYTE (uint32_t)(&_END_OF_FLASH)
 #define DEVICE_ID_FLASH_SIZE ((uint16_t*)0x1fff7a22)
 
 static const uint8_t flash_sector[] = {
-    FLASH_Sector_0,
-    FLASH_Sector_1,
-    FLASH_Sector_2,
-    FLASH_Sector_3,
-    FLASH_Sector_4,
-    FLASH_Sector_5,
-    FLASH_Sector_6,
-    FLASH_Sector_7,
-    FLASH_Sector_8,
-    FLASH_Sector_9,
-    FLASH_Sector_10,
-    FLASH_Sector_11,
-    FLASH_Sector_12,
-    FLASH_Sector_13,
-    FLASH_Sector_14,
-    FLASH_Sector_15,
-    FLASH_Sector_16,
-    FLASH_Sector_17,
-    FLASH_Sector_18,
-    FLASH_Sector_19,
-    FLASH_Sector_20,
-    FLASH_Sector_21,
-    FLASH_Sector_22,
-    FLASH_Sector_23
+	FLASH_Sector_0,
+	FLASH_Sector_1,
+	FLASH_Sector_2,
+	FLASH_Sector_3,
+	FLASH_Sector_4,
+	FLASH_Sector_5,
+	FLASH_Sector_6,
+	FLASH_Sector_7,
+	FLASH_Sector_8,
+	FLASH_Sector_9,
+	FLASH_Sector_10,
+	FLASH_Sector_11,
+	FLASH_Sector_12,
+	FLASH_Sector_13,
+	FLASH_Sector_14,
+	FLASH_Sector_15,
+	FLASH_Sector_16,
+	FLASH_Sector_17,
+	FLASH_Sector_18,
+	FLASH_Sector_19,
+	FLASH_Sector_20,
+	FLASH_Sector_21,
+	FLASH_Sector_22,
+	FLASH_Sector_23
 };
 
 static const uint32_t sector_boundaries[] = {
-    0x08004000,
-    0x08008000,
-    0x0800c000,
-    0x08010000,
-    0x08020000,
-    0x08040000,
-    0x08060000,
-    0x08080000,
-    0x080a0000,
-    0x080c0000,
-    0x080e0000,
-    0x08100000,
-    // STM32F42/STM32F43
-    0x08104000,
-    0x08108000,
-    0x0810c000,
-    0x08110000,
-    0x08120000,
-    0x08140000,
-    0x08160000,
-    0x08180000,
-    0x081a0000,
-    0x081c0000,
-    0x081e0000,
-    0x08200000
+	0x08004000,
+	0x08008000,
+	0x0800c000,
+	0x08010000,
+	0x08020000,
+	0x08040000,
+	0x08060000,
+	0x08080000,
+	0x080a0000,
+	0x080c0000,
+	0x080e0000,
+	0x08100000,
+	// STM32F42/STM32F43
+	0x08104000,
+	0x08108000,
+	0x0810c000,
+	0x08110000,
+	0x08120000,
+	0x08140000,
+	0x08160000,
+	0x08180000,
+	0x081a0000,
+	0x081c0000,
+	0x081e0000,
+	0x08200000
 };
 
 static uint32_t flash_size_bytes() {
-    return (*DEVICE_ID_FLASH_SIZE) * 1024;
+	return (*DEVICE_ID_FLASH_SIZE) * 1024;
 }
 
 static uint32_t highest_flash_address() {
-    return 0x08000000 + flash_size_bytes() - 1;
+	return 0x08000000 + flash_size_bytes() - 1;
 }
 
 static uint8_t sector_id_for_address(uint32_t address) {
-    if (address < 0x08000000) return 0xff;
+	if (address < 0x08000000) return 0xff;
 
-    for (uint8_t i = 0; i < 24; i++) {
-        if (address < sector_boundaries[i]) return i;
-    }
+	for (uint8_t i = 0; i < 24; i++) {
+		if (address < sector_boundaries[i]) return i;
+	}
 
-    return 0xff;
+	return 0xff;
 }
 
 static uint32_t lowest_available_flash_address() {
-    uint8_t firmware_size = FIRST_FREE_BYTE - 0x08000000;
+	uint8_t firmware_size = FIRST_FREE_BYTE - 0x08000000;
 
-    return (RESERVED_FLASH_KB * 1024) > firmware_size ?
-        (0x08000000 + RESERVED_FLASH_KB * 1024) :
-        FIRST_FREE_BYTE;
+	return (RESERVED_FLASH_KB * 1024) > firmware_size ?
+		(0x08000000 + RESERVED_FLASH_KB * 1024) :
+		FIRST_FREE_BYTE;
 }
 
 uint32_t available_flash() {
-    uint8_t last_reserved_sector = sector_id_for_address(lowest_available_flash_address() - 1);
-    uint8_t highest_available_sector = sector_id_for_address(highest_flash_address());
+	uint8_t last_reserved_sector = sector_id_for_address(lowest_available_flash_address() - 1);
+	uint8_t highest_available_sector = sector_id_for_address(highest_flash_address());
 
-    if (last_reserved_sector == 0xff || highest_available_sector == 0xff) return 0;
+	if (last_reserved_sector == 0xff || highest_available_sector == 0xff) return 0;
 
-    return sector_boundaries[highest_available_sector] - sector_boundaries[last_reserved_sector];
+	return sector_boundaries[highest_available_sector] - sector_boundaries[last_reserved_sector];
 }
 
 bool prepare_flash(uint32_t size, flash_context* context) {
-    if (size > available_flash() || size == 0) return false;
+	if (size > available_flash() || size == 0) return false;
 
-    const uint8_t first_sector_id = sector_id_for_address(highest_flash_address() - size + 1);
-    const uint8_t last_sector_id = sector_id_for_address(highest_flash_address());
+	const uint8_t first_sector_id = sector_id_for_address(highest_flash_address() - size + 1);
+	const uint8_t last_sector_id = sector_id_for_address(highest_flash_address());
 
-    if (first_sector_id == 0xff || last_sector_id == 0xff) return false;
-    if (first_sector_id <= sector_id_for_address(lowest_available_flash_address() - 1)) return false;
+	if (first_sector_id == 0xff || last_sector_id == 0xff) return false;
+	if (first_sector_id <= sector_id_for_address(lowest_available_flash_address() - 1)) return false;
 
-    FLASH_Unlock();
-    if (FLASH_WaitForLastOperation() != FLASH_COMPLETE) goto error;
+	FLASH_Unlock();
+	if (FLASH_WaitForLastOperation() != FLASH_COMPLETE) goto error;
 
-    for (uint8_t sector_id = first_sector_id; sector_id <= last_sector_id; sector_id++) {
-        if (FLASH_EraseSector(flash_sector[sector_id], VoltageRange_3) != FLASH_COMPLETE) goto error;
-    }
+	for (uint8_t sector_id = first_sector_id; sector_id <= last_sector_id; sector_id++) {
+		if (FLASH_EraseSector(flash_sector[sector_id], VoltageRange_3) != FLASH_COMPLETE) goto error;
+	}
 
-    FLASH_Lock();
+	FLASH_Lock();
 
-    context->next_write_target = highest_flash_address() - size + 1;
-    context->base = (uint8_t*)(context->next_write_target);
+	context->next_write_target = highest_flash_address() - size + 1;
+	context->base = (uint8_t*)(context->next_write_target);
 
-    return true;
+	return true;
 
-    error:
-        FLASH_Lock();
-        return false;
+	error:
+		FLASH_Lock();
+		return false;
 }
 
 bool write_flash(uint32_t byte_count, uint8_t* buffer, flash_context *context) {
-    if (context->next_write_target < lowest_available_flash_address()) return false;
-    if (context->next_write_target + byte_count - 1 > highest_flash_address()) return false;
+	if (context->next_write_target < lowest_available_flash_address()) return false;
+	if (context->next_write_target + byte_count - 1 > highest_flash_address()) return false;
 
-    FLASH_Unlock();
-    if (FLASH_WaitForLastOperation() != FLASH_COMPLETE) goto error;
+	FLASH_Unlock();
+	if (FLASH_WaitForLastOperation() != FLASH_COMPLETE) goto error;
 
-    if (((uint32_t)buffer & 0x03) == 0x00 && (context->next_write_target & 0x03) == 0x00 && (byte_count & 0x03) == 0x00) {
-        for (uint32_t i = 0; i < byte_count >> 2; i++) {
-            if (FLASH_ProgramWord(context->next_write_target, *((uint32_t*)buffer)) != FLASH_COMPLETE)
-                goto error;
+	if (((uint32_t)buffer & 0x03) == 0x00 && (context->next_write_target & 0x03) == 0x00 && (byte_count & 0x03) == 0x00) {
+		for (uint32_t i = 0; i < byte_count >> 2; i++) {
+			if (FLASH_ProgramWord(context->next_write_target, *((uint32_t*)buffer)) != FLASH_COMPLETE)
+				goto error;
 
-            context->next_write_target += 4;
-            buffer += 4;
-        }
-    }
-    else if (((uint32_t)buffer & 0x01) == 0x00 && (context->next_write_target & 0x01) == 0x00 && (byte_count & 0x01) == 0) {
-        for (uint32_t i = 0; i < byte_count >> 1; i++) {
-            if (FLASH_ProgramHalfWord(context->next_write_target, *((uint16_t*)buffer)) != FLASH_COMPLETE)
-                goto error;
+			context->next_write_target += 4;
+			buffer += 4;
+		}
+	}
+	else if (((uint32_t)buffer & 0x01) == 0x00 && (context->next_write_target & 0x01) == 0x00 && (byte_count & 0x01) == 0) {
+		for (uint32_t i = 0; i < byte_count >> 1; i++) {
+			if (FLASH_ProgramHalfWord(context->next_write_target, *((uint16_t*)buffer)) != FLASH_COMPLETE)
+				goto error;
 
-            context->next_write_target += 2;
-            buffer += 2;
-        }
-    }
-    else  {
-        for (uint32_t i = 0; i < byte_count; i++) {
-            if (FLASH_ProgramByte(context->next_write_target, *buffer) != FLASH_COMPLETE)
-                goto error;
+			context->next_write_target += 2;
+			buffer += 2;
+		}
+	}
+	else  {
+		for (uint32_t i = 0; i < byte_count; i++) {
+			if (FLASH_ProgramByte(context->next_write_target, *buffer) != FLASH_COMPLETE)
+				goto error;
 
-            context->next_write_target++;
-            buffer++;
-        }
-    }
+			context->next_write_target++;
+			buffer++;
+		}
+	}
 
-    FLASH_Lock();
-    return true;
+	FLASH_Lock();
+	return true;
 
-    error:
-        FLASH_Lock();
-        return false;
+	error:
+		FLASH_Lock();
+		return false;
 }
